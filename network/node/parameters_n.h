6f0202ef662b07a7b4e7a76e9422f98e73a8bdf4
dfurle <denis.furletov@gmail.com>
2023-07-19 06:39:21 -0400

    revamp workspace
    now will need to copy paste the new hls4ml generated code in the files

---
+++ network/node/parameters_n.h
@@ -0,0 +1,176 @@
+#ifndef PARAMETERS_H_NODE
+#define PARAMETERS_H_NODE
+
+#include "ap_fixed.h"
+#include "ap_int.h"
+
+#include "nnet_utils/nnet_code_gen.h"
+#include "nnet_utils/nnet_helpers.h"
+// hls-fpga-machine-learning insert includes
+#include "nnet_utils/nnet_activation.h"
+#include "nnet_utils/nnet_activation_stream.h"
+#include "nnet_utils/nnet_dense.h"
+#include "nnet_utils/nnet_dense_compressed.h"
+#include "nnet_utils/nnet_dense_stream.h"
+
+namespace node_net{
+
+// hls-fpga-machine-learning insert weights
+#define __SYNTHESIS__
+#include "weights_n/w2.h"
+#include "weights_n/b2.h"
+#include "weights_n/w4.h"
+#include "weights_n/b4.h"
+#include "weights_n/w6.h"
+#include "weights_n/b6.h"
+#include "weights_n/w8.h"
+#include "weights_n/b8.h"
+#include "weights_n/w10.h"
+#include "weights_n/b10.h"
+#undef __SYNTHESIS__
+
+// hls-fpga-machine-learning insert layer-config
+// dense_node_1
+struct config2 : nnet::dense_config {
+    static const unsigned n_in = 30;
+    static const unsigned n_out = 8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 180;
+    static const unsigned n_nonzeros = 60;
+    static const unsigned multiplier_limit = DIV_ROUNDUP(n_in * n_out, reuse_factor) - n_zeros / reuse_factor;
+    static const bool store_weights_in_bram = false;
+    typedef model_default_t accum_t;
+    typedef dense_node_1_bias_t bias_t;
+    typedef dense_node_1_weight_t weight_t;
+    typedef layer2_index index_t;
+    template<class x_T, class y_T>
+    using product = nnet::product::mult<x_T, y_T>;
+};
+
+// dense_node_1_relu
+struct relu_config3 : nnet::activ_config {
+    static const unsigned n_in = 8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef dense_node_1_relu_table_t table_t;
+};
+
+// dense_node_2
+struct config4 : nnet::dense_config {
+    static const unsigned n_in = 8;
+    static const unsigned n_out = 8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 48;
+    static const unsigned n_nonzeros = 16;
+    static const unsigned multiplier_limit = DIV_ROUNDUP(n_in * n_out, reuse_factor) - n_zeros / reuse_factor;
+    static const bool store_weights_in_bram = false;
+    typedef model_default_t accum_t;
+    typedef dense_node_2_bias_t bias_t;
+    typedef dense_node_2_weight_t weight_t;
+    typedef layer4_index index_t;
+    template<class x_T, class y_T>
+    using product = nnet::product::mult<x_T, y_T>;
+};
+
+// dense_node_2_relu
+struct relu_config5 : nnet::activ_config {
+    static const unsigned n_in = 8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef dense_node_2_relu_table_t table_t;
+};
+
+// dense_node_3
+struct config6 : nnet::dense_config {
+    static const unsigned n_in = 8;
+    static const unsigned n_out = 8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 48;
+    static const unsigned n_nonzeros = 16;
+    static const unsigned multiplier_limit = DIV_ROUNDUP(n_in * n_out, reuse_factor) - n_zeros / reuse_factor;
+    static const bool store_weights_in_bram = false;
+    typedef model_default_t accum_t;
+    typedef dense_node_3_bias_t bias_t;
+    typedef dense_node_3_weight_t weight_t;
+    typedef layer6_index index_t;
+    template<class x_T, class y_T>
+    using product = nnet::product::mult<x_T, y_T>;
+};
+
+// dense_node_3_relu
+struct relu_config7 : nnet::activ_config {
+    static const unsigned n_in = 8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef dense_node_3_relu_table_t table_t;
+};
+
+// dense_node_4
+struct config8 : nnet::dense_config {
+    static const unsigned n_in = 8;
+    static const unsigned n_out = 8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 48;
+    static const unsigned n_nonzeros = 16;
+    static const unsigned multiplier_limit = DIV_ROUNDUP(n_in * n_out, reuse_factor) - n_zeros / reuse_factor;
+    static const bool store_weights_in_bram = false;
+    typedef model_default_t accum_t;
+    typedef dense_node_4_bias_t bias_t;
+    typedef dense_node_4_weight_t weight_t;
+    typedef layer8_index index_t;
+    template<class x_T, class y_T>
+    using product = nnet::product::mult<x_T, y_T>;
+};
+
+// dense_node_4_relu
+struct relu_config9 : nnet::activ_config {
+    static const unsigned n_in = 8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef dense_node_4_relu_table_t table_t;
+};
+
+// dense_node_out
+struct config10 : nnet::dense_config {
+    static const unsigned n_in = 8;
+    static const unsigned n_out = 8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 48;
+    static const unsigned n_nonzeros = 16;
+    static const unsigned multiplier_limit = DIV_ROUNDUP(n_in * n_out, reuse_factor) - n_zeros / reuse_factor;
+    static const bool store_weights_in_bram = false;
+    typedef model_default_t accum_t;
+    typedef dense_node_out_bias_t bias_t;
+    typedef dense_node_out_weight_t weight_t;
+    typedef layer10_index index_t;
+    template<class x_T, class y_T>
+    using product = nnet::product::mult<x_T, y_T>;
+};
+
+// dense_node_out_relu
+struct relu_config11 : nnet::activ_config {
+    static const unsigned n_in = 8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef dense_node_out_relu_table_t table_t;
+};
+
+}
+
+
+#endif
---
+++ runner/runner.cpp
@@ -0,0 +1,147 @@
+#include "myproject_i.h"
+#include "myproject_e.h"
+#include "myproject_n.h"
+
+#include "projectDefines.h"
+
+#include <iostream>
+
+extern "C"{
+
+void runner(data_t X_arr[NHITS * NPARAMS], R_data_t Ro_arr[NHITS * NEDGES], R_data_t Ri_arr[NHITS * NEDGES], data_t e_arr[NEDGES]){
+
+  #pragma HLS INTERFACE m_axi port=X_arr  offset=slave bundle=aximm1
+  #pragma HLS INTERFACE m_axi port=Ro_arr offset=slave bundle=aximm1
+  #pragma HLS INTERFACE m_axi port=Ri_arr offset=slave bundle=aximm1
+  #pragma HLS INTERFACE m_axi port=e_arr  offset=slave bundle=aximm1
+  // #pragma HLS INTERFACE s_axilite port=X_arr  bundle=control
+  // #pragma HLS INTERFACE s_axilite port=Ro_arr bundle=control
+  // #pragma HLS INTERFACE s_axilite port=Ri_arr bundle=control
+  // #pragma HLS INTERFACE s_axilite port=e_arr  bundle=control
+  // #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+  // #pragma HLS ARRAY_RESHAPE variable = X_arr complete dim = 0
+  // #pragma HLS ARRAY_RESHAPE variable = Ro_arr complete dim = 0
+  // #pragma HLS ARRAY_RESHAPE variable = Ri_arr complete dim = 0
+  // #pragma HLS ARRAY_RESHAPE variable = e_arr complete dim = 0
+
+  #ifdef STREAM
+  H_t H;
+  X_t X;
+  R_t Ro, Ri;
+  e_t e;
+
+  data_t* ptr = (data_t*) X.begin();
+  ptr = X_arr;
+
+  hls::stream<H_t> stream_H2;
+  hls::stream<H_t> stream_H;
+  hls::stream<X_t> stream_X;
+  hls::stream<R_t> stream_Ro;
+  hls::stream<R_t> stream_Ri;
+  hls::stream<e_t> stream_e;
+
+  stream_X << X;
+  input_network(stream_X, stream_H);
+
+
+  // ITER #1
+  stream_H >> H;
+
+  stream_H << H;
+  stream_Ro << Ro;
+  stream_Ri << Ri;
+  edge_network(stream_H, stream_Ro, stream_Ri, stream_e);
+
+  stream_H << H;
+  stream_Ro << Ro;
+  stream_Ri << Ri;
+  node_network(stream_H, stream_Ro, stream_Ri, stream_e, stream_H2);
+
+  // ITER #2
+  stream_H2 >> H;
+
+  stream_H << H;
+  stream_Ro << Ro;
+  stream_Ri << Ri;
+  edge_network(stream_H, stream_Ro, stream_Ri, stream_e);
+
+  stream_H << H;
+  stream_Ro << Ro;
+  stream_Ri << Ri;
+  node_network(stream_H, stream_Ro, stream_Ri, stream_e, stream_H2);
+
+  // Ending
+
+  stream_H2 >> H;
+
+  stream_H << H;
+  stream_Ro << Ro;
+  stream_Ri << Ri;
+  edge_network(stream_H, stream_Ro, stream_Ri, stream_e);
+
+  stream_e >> e;
+
+  for(int i = 0; i < NEDGES; i++){
+    e_arr[i] = e[i];
+  }
+  #endif
+
+  #ifdef VECTOR
+  H_t H, H2;
+  X_t X;
+  R_t Ro, Ri;
+  e_t e;
+
+
+  for(int i = 0; i < NHITS; i++){
+    for(int j = 0; j < NPARAMS; j++){
+      X[i][j] = X_arr[i*NPARAMS + j];
+    }
+    for(int j = 0; j < NEDGES; j++){
+      Ro[i][j] = Ro_arr[i*NEDGES + j];
+      Ri[i][j] = Ri_arr[i*NEDGES + j];
+    }
+  }
+
+  input_network(X, H);
+
+  std::cout << "H: " << std::endl;
+  for(int i = 0; i < NHITS; i++){
+    for(int j = 0; j < NPARHID; j++){
+      std::cout << float(H[i][j]) << std::endl;
+    }
+  }
+
+  // ITER #1
+  edge_network(H, Ro, Ri, e);
+  node_network(H, Ro, Ri, e, H2);
+  // ITER #2
+  edge_network(H2, Ro, Ri, e);
+  node_network(H2, Ro, Ri, e, H);
+  // Ending
+  edge_network(H, Ro, Ri, e);
+
+  for(int i = 0; i < NEDGES; i++){
+    e_arr[i] = e[i];
+  }
+  #endif
+
+  #ifdef ARRAY
+  data_t H_arr[NHITS * NPARHID];
+  data_t H2_arr[NHITS * NPARHID];
+
+  input_network(X_arr, H_arr);
+  // ITER #1
+  edge_network(H_arr, Ro_arr, Ri_arr, e_arr);
+  node_network(H_arr, Ro_arr, Ri_arr, e_arr, H2_arr);
+  // ITER #2
+  edge_network(H2_arr, Ro_arr, Ri_arr, e_arr);
+  node_network(H2_arr, Ro_arr, Ri_arr, e_arr, H_arr);
+  // Ending
+  edge_network(H_arr, Ro_arr, Ri_arr, e_arr);
+  #endif
+  
+}
+
+}
